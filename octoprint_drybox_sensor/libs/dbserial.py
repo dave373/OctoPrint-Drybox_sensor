f922c0e5b24d29b697b5c8a8feda67140e21bd94
David Haddon <dave373@gmail.com>
2024-07-01 16:08:08 +1000

    not useable?? pushing just in case

--- octoprint_drybox_sensor/libs/dbserial.py
+++ octoprint_drybox_sensor/libs/dbserial.py
@@ -5,185 +5,253 @@ import random
 import json
 from pathlib import Path
 
+
 class DBSerial(threading.Thread):
 
-  HISTORY_FILE = "/home/pi/.octoprint/data/drybox/history.json"
+    HISTORY_FILE = "/home/pi/.octoprint/data/drybox/history.json"
 
-  def __init__(self, port, dbpi = None):
-    threading.Thread.__init__(self)
-    self._port = port
-    self.logger = None
-    self.pm = None
-    self.dbpi = dbpi
-    if dbpi is not None:
-      self.logger = dbpi._logger
-      self.pm = dbpi._plugin_manager
-      self.dbpi = dbpi
-    self.temp = 0
-    self.humid = 0
-    self.ph = None
-    self.history = []
-    self.hist_length = 115200  # 4 days at 1 data point/5 secs
-    self.hist_delay = 0 # capture everything
-    self.send_history = False
-    if not Path(self.HISTORY_FILE).parent.is_dir():
-      Path(self.HISTORY_FILE).parent.mkdir(parents=True, exist_ok=True)
+    def __init__(self, port, dbpi=None):
+        threading.Thread.__init__(self)
+        self._port = port
+        self.logger = None
+        self.pm = None
+        self.dbpi = dbpi
+        if dbpi is not None:
+            self.logger = dbpi._logger
+            self.pm = dbpi._plugin_manager
+            self.dbpi = dbpi
+        self.temp = 0
+        self.humid = 0
+        self.ts = 0
+        self.ph = None
+        self.history = []
+        self.hist_length = 115200  # 4 days at 1 data point/5 secs
+        self.hist_delay = 0  # capture everything
+        self.send_history = False
+        if not Path(self.HISTORY_FILE).parent.is_dir():
+            Path(self.HISTORY_FILE).parent.mkdir(parents=True, exist_ok=True)
 
-  def getTemp(self):
-    if self.ph is not None:
-      return self.temp
-    else:
-      raise ConnectionError("Not connected")
-
-  def getHumid(self):
-    if self.ph is not None:
-      return self.humid
-    else:
-      raise ConnectionError("Not connected")
-    
-  def log(self, txt):
-    if self.logger is not None:
-      self.logger.info(" ::DBSerial:: %s" %txt)
-    else:
-      print(txt)
-
-  def write_data_file(self):
-    try:
-      with open(self.HISTORY_FILE,"w") as fh:
-        json.dump(self.history, fh, indent=2)
-    except Exception as e:
-        self.log("Failed to write data file : %s" %e)
-
-  def read_data_file(self):
-    try:
-      with open(self.HISTORY_FILE,"r") as fh:
-        self.history = json.load(fh)
-    except Exception as e:
-        self.log("Failed to read data file : %s" %e)
-
-  def open(self):
-    try:
-      if self._port == "debug":
-        self.ph = "debug"
-        self.log("DEBUG endpoint is being used")
-      else:
-        self.ph = serial.Serial(self._port, timeout=10)
-        self.log("Opened the port : %s" %self._port)
-    except Exception as e:
-      self.log("Failed to open sensor port : %s" %e)
-      self.ph = None
-      return False
-    return self.ph
-    
-  def close(self):
-    if len(self.history) > 1000:
-      self.log("Writing history file before closing")
-      self.write_data_file()
-    else :
-      self.log("history is short, not saving")
-    if self.ph != "debug":
-      self.ph.close()
-
-  def readData(self):
-    if self.ph == "debug":
-      time.sleep(3)
-      self.temp = random.random()*30+20
-      self.humid = random.random()*50+10
-    else:
-      #self.log("Waiting for data...")
-      try:
-        dstr = self.ph.readline().decode()
-      except Exception as e:
-        self.log("Exception reading data.. " , e)
-        self.temp = -2
-        self.humid = -2
-        return False
-      #self.log("Got data: %s" %dstr)
-      data = dstr.strip().split(",")
-      self.temp = float(data[0][2:7])
-      self.humid = float(data[1][2:7])
-    return True
-
-  def run(self):
-    try:
-      self.done = False
-      self.log("Loading history data file")
-      self.read_data_file()
-
-      if self.open() is not False:
-        self.log("Starting Serial read thread")
-        while not self.done:
-          #self.log("Reading data in thread")
-          if self.readData():
-            try:
-              self.addHistData()
-              if (time.time() - Path(self.HISTORY_FILE).stat().st_mtime) > 86400:
-                self.write_data_file()
-             #self.log("Send_history is %s, AddHistData returned %s" %(self.send_history, sendhist))
-              if self.pm is not None:
-                if self.send_history:
-                  #self.log("Sending PM message with history %0.2f %0.2f Hist:[%d]" %(self.temp,self.humid,len(self.history)))
-                  self.pm.send_plugin_message(
-                    self.dbpi._identifier, dict(temp=self.temp, humid=self.humid, history=self.history)
-                  )
-                  self.send_history = False
-                else:
-                  #self.log("Sending PM message %0.2f %0.2f" %(self.temp,self.humid))
-                  self.pm.send_plugin_message(
-                    self.dbpi._identifier, dict(temp=self.temp, humid=self.humid)
-                  )
-            except Exception as e:
-              self.log("Failed to read or send data in run loop : %s" %e)
-      else :
-        # Failed to open port
-        if self.pm is not None:
-          self.pm.send_plugin_message(self.dbpi._identifier, dict(temp=-1,humid=-1))
+    def getTemp(self):
+        if self.ph is not None:
+            return self.temp
         else:
-          self.log("Failed to open port : %s" %self._port)
-        self.done=True
-    except KeyboardInterrupt:
-      self.log("Caught a ctrl-C... shutdown time")
-      self.done=True
-    self.close() 
+            raise ConnectionError("Not connected")
 
-  def stop(self):
-    self.done = True
+    def getHumid(self):
+        if self.ph is not None:
+            return self.humid
+        else:
+            raise ConnectionError("Not connected")
 
-  def addHistData(self):
-    if len(self.history) == 0:
-      # initial entry
-      self.history.append({"ts":time.time(),"temp":self.temp,"humid":self.humid})
-      return True
-    else:
-      lastData = self.history[-1]["ts"]
-      if (lastData + self.hist_delay) < time.time():
-        # Store the new value
-        self.history.append({"ts":time.time(),"temp":self.temp,"humid":self.humid})
-        if len(self.history) > self.hist_length:
-          self.history.pop(0)
+    def log(self, txt):
+        if self.logger is not None:
+            self.logger.info(" ::DBSerial:: %s" % txt)
+        else:
+            print(txt)
+
+    def write_data_file(self):
+        try:
+            with open(self.HISTORY_FILE, "w") as fh:
+                json.dump(self.history, fh, indent=2)
+        except Exception as e:
+            self.log("Failed to write data file : %s" % e)
+
+    def read_data_file(self):
+        try:
+            with open(self.HISTORY_FILE, "r") as fh:
+                self.history = json.load(fh)
+                self.log(
+                    "Loaded history data from file of %d DPs from %s to %s"
+                    % (
+                        len(self.history),
+                        time.ctime(self.history[0]["ts"]),
+                        time.ctime(self.history[-1]["ts"]),
+                    )
+                )
+        except Exception as e:
+            self.log("Failed to read data file : %s" % e)
+
+    def open(self):
+        try:
+            if self._port == "debug":
+                self.ph = "debug"
+                self.log("DEBUG endpoint is being used")
+            else:
+                self.ph = serial.Serial(self._port, timeout=10)
+                self.log("Opened the port : %s" % self._port)
+        except Exception as e:
+            self.log("Failed to open sensor port : %s" % e)
+            self.ph = None
+            return False
+        return self.ph
+
+    def close(self):
+        if len(self.history) > 1000:
+            self.log("Writing history file before closing")
+            self.write_data_file()
+        else:
+            self.log("history is short, not saving")
+        if self.ph != "debug":
+            self.ph.close()
+
+    def read_data(self):
+        if self.ph == "debug":
+            time.sleep(3)
+            self.temp = random.random() * 30 + 20
+            self.humid = random.random() * 50 + 10
+            self.ts = time.time()
+        else:
+            # self.log("Waiting for data...")
+            try:
+                dstr = self.ph.readline().decode()
+            except Exception as e:
+                self.log("Exception reading data.. ", e)
+                self.temp = -2
+                self.humid = -2
+                return False
+            # self.log("Got data: %s" %dstr)
+            data = dstr.strip().split(",")
+            self.temp = float(data[0][2:7])
+            self.humid = float(data[1][2:7])
+            self.ts = time.time()
         return True
-      #else :
-      #  self.log("Next history update in %0.0f seconds" %((lastData + self.hist_delay)-time.time()))
-    return False
-    
 
-if __name__ == "__main__":
+    def get_history_data(self, tspan, count=100):
+        tspan = int(tspan)
+        if count == 0:
+            count = 100
+        self.log("Getting data from tspan of %d" % tspan)
+        results = []
+        if not tspan:
+            self.log("No tspan specified, so returning from all history")
+            results = self.history[0 :: int(len(self.history) / count)]
+        else:
+            now = time.time()
+            rec = time.time() - tspan
+            self.log("NOW: %d   REC=%d  diff=%d" % (now, rec, now - rec))
+            index = 0
 
-  dbs = DBSerial('debug')
+            while int(self.history[index]["ts"]) < rec:
+                index += 1
+                if index >= len(self.history):
+                    self.log(
+                        "Reached the end of the history... How??  len=%d, ind=%d, rec=%d, last_ts=%d"
+                        % (len(self.history), index, rec, self.history[index - 1]["ts"])
+                    )
+                    index -= 1
+                    break
+            if (count*2) <= len(self.history[index:]):
+                self.log(
+                    "Returning %d history points from %d available, from %d to %d"
+                    % (count, len(self.history) - index, self.history[index]["ts"], now)
+                )
+                return self.history[index :: int(len(self.history[index:]) / count)]
+            else:
+                self.log(
+                    "Returning all history points from the %d available, from %d to %d"
+                    % (len(self.history) - index, self.history[index]["ts"], now)
+                )
+                return self.history[index:]
 
-  dbs.start()
-  time.sleep(1)
-  print("Starting test")      
-  while 1:
-    try:
-      print ("Temp: %0.2f   Humid: %0.2f  History length=%d" %(dbs.getTemp(),dbs.getHumid(),len(dbs.history)))
-      time.sleep(5)
-      
-    except KeyboardInterrupt:
-      break
-    except Exception as e:
-      print("Something went wrong : %s" %e)
-      break
-  print("Waiting for thread to stop")
-  dbs.stop()
-  dbs.join()
+
+    def run(self):
+        try:
+            self.done = False
+            self.log("Loading history data file")
+            self.read_data_file()
+
+            if self.open() is not False:
+                self.log("Starting Serial read thread")
+                while not self.done:
+                    # self.log("Reading data in thread")
+                    if self.read_data():
+                        try:
+                            self.add_hist_data()
+                            if (
+                                time.time() - Path(self.HISTORY_FILE).stat().st_mtime
+                            ) > 86400:
+                                self.write_data_file()
+                            # self.log("Send_history is %s, AddHistData returned %s" %(self.send_history, sendhist))
+                            if self.pm is not None:
+                                #if self.send_history:
+                                #    # self.log("Sending PM message with history %0.2f %0.2f Hist:[%d]" %(self.temp,self.humid,len(self.history)))
+                                #    self.pm.send_plugin_message(
+                                #        self.dbpi._identifier,
+                                #        dict(
+                                #            temp=self.temp,
+                                #            humid=self.humid,
+                                #            history=self.history,
+                                #        ),
+                                #    )
+                                #    self.send_history = False
+                            #    else:
+                                    # self.log("Sending PM message %0.2f %0.2f" %(self.temp,self.humid))
+                                self.pm.send_plugin_message(
+                                    self.dbpi._identifier,
+                                    dict(
+                                        ts=self.ts, temp=self.temp, humid=self.humid
+                                    ),
+                                )
+                        except Exception as e:
+                            self.log("Failed to read or send data in run loop : %s" % e)
+            else:
+                # Failed to open port
+                if self.pm is not None:
+                    self.pm.send_plugin_message(
+                        self.dbpi._identifier, dict(temp=-1, humid=-1)
+                    )
+                else:
+                    self.log("Failed to open port : %s" % self._port)
+                self.done = True
+        except KeyboardInterrupt:
+            self.log("Caught a ctrl-C... shutdown time")
+            self.done = True
+        self.close()
+
+    def stop(self):
+        self.done = True
+
+    def add_hist_data(self):
+        if len(self.history) == 0:
+            # initial entry
+            self.history.append(
+                {"ts": time.time(), "temp": self.temp, "humid": self.humid}
+            )
+            return True
+        else:
+            lastData = self.history[-1]["ts"]
+            if (lastData + self.hist_delay) < time.time():
+                # Store the new value
+                self.history.append(
+                    {"ts": time.time(), "temp": self.temp, "humid": self.humid}
+                )
+                if len(self.history) > self.hist_length:
+                    self.history.pop(0)
+                return True
+            # else :
+            #  self.log("Next history update in %0.0f seconds" %((lastData + self.hist_delay)-time.time()))
+        return False
+
+    if __name__ == "__main__":
+
+        dbs = DBSerial("debug")
+
+        dbs.start()
+        time.sleep(1)
+        print("Starting test")
+        while 1:
+            try:
+                print(
+                    "Temp: %0.2f   Humid: %0.2f  History length=%d"
+                    % (dbs.getTemp(), dbs.getHumid(), len(dbs.history))
+                )
+                time.sleep(5)
+
+            except KeyboardInterrupt:
+                break
+            except Exception as e:
+                print("Something went wrong : %s" % e)
+                break
+        print("Waiting for thread to stop")
+        dbs.stop()
+        dbs.join()
--- octoprint_drybox_sensor/static/js/drybox_sensor.js
+++ octoprint_drybox_sensor/static/js/drybox_sensor.js
@@ -47,21 +47,236 @@ $(function() {
           if (data.temp > self.settings.settings.plugins.drybox_sensor.temp_error()) {
             $('#drybox-temp').addClass("drybox_error");
           }
-          else if (data.temp > self.settings.settings.plugins.drybox_sensor.temp_warn()) {
-            $('#drybox-temp').addClass("drybox_warn");
+	        if (data.temp == -1 && data.humid == -1) {
+	        // Connection error. 
+            $('#drybox-temp').html("Not Connected").addClass("drybox_error");
+            $('#drybox-humid').hide();
           }
-          
+          else if (data.temp == -2 && data.humid == -2) {
+            // Data Timeout error. 
+              $('#drybox-temp').html("Data Timeout").addClass("drybox_error");
+              $('#drybox-humid').hide();
+          }  
+          else {
+            $('#drybox-humid').show();
+            if (data.temp) {
+              $('#drybox-temp').html(_.sprintf(":%.1f&deg;C", data.temp)).removeClass("drybox_error drybox_warn");
+              if (data.temp > self.settings.settings.plugins.drybox_sensor.temp_error()) {
+                $('#drybox-temp').addClass("drybox_error");
+              }
+              else if (data.temp > self.settings.settings.plugins.drybox_sensor.temp_warn()) {
+                $('#drybox-temp').addClass("drybox_warn");
+              }
+              
+            }
+            if (data.humid) {
+              $('#drybox-humid').html(_.sprintf("%.1f%%", data.humid)).removeClass("drybox_error drybox_warn");
+              if (data.humid > self.settings.settings.plugins.drybox_sensor.humid_error()) {
+                $('#drybox-humid').addClass("drybox_error");
+              }
+              else if (data.humid > self.settings.settings.plugins.drybox_sensor.humid_warn()) {
+                $('#drybox-humid').addClass("drybox_warn");
+              }
+              
+            }
+          }
+
+
         }
-        if (data.humid) {
-          $('#drybox-humid').html(_.sprintf("%.1f%%", data.humid)).removeClass("drybox_error drybox_warn");
-          if (data.humid > self.settings.settings.plugins.drybox_sensor.humid_error()) {
-            $('#drybox-humid').addClass("drybox_error");
-          }
-          else if (data.humid > self.settings.settings.plugins.drybox_sensor.humid_warn()) {
-            $('#drybox-humid').addClass("drybox_warn");
-          }
-          
+        
+         self.printTextHistory = function() {
+            html = "<ul class='drybox-histlist'>";
+            var textskip = Math.floor(data.history.length/50);
+            var count = 0;
+            for (h in data.history) {
+              var d = new Date(data.history[h]['ts']*1000);
+              if (count%textskip == 0) {
+                 
+                  html += "<li>" + d.toLocaleString() + "  ";
+                  tclass = "";
+                  if (data.history[h]['temp'] > parseInt(self.settings.settings.plugins.drybox_sensor.temp_error())) {
+                    tclass = "drybox_error";
+                  }
+                  else if (data.history[h]['temp'] > parseInt(self.settings.settings.plugins.drybox_sensor.temp_warn())) {
+                    tclass = "drybox_warn"; 
+                  }
+                  
+                  html += "<b class='" + tclass + "'>" + _.sprintf("%3.1f&deg;C", data.history[h]['temp']) + "</b>";
+                  hclass = "";
+                  if (data.history[h]['humid'] > self.settings.settings.plugins.drybox_sensor.humid_error()) {
+                    hclass = "drybox_error";
+                  }
+                  else if (data.history[h]['humid'] > self.settings.settings.plugins.drybox_sensor.humid_warn()) {
+                    hclass = "drybox_warn";
+                  }
+                                    html += "<b class='" + hclass + "'>" + _.sprintf("%3.1f%%", data.history[h]['humid']) + "</b></li>\n";
+              }
+              count++;
+            }
+            html += "</ul>"
+            $('#drybox-history').html(html);
         }
+       
+
+        self.onBeforeBinding = function() {
+          //self.settings = self.global_settings.settings.plugins.drybox_sensor;
+          console.log("DRYBOX: On Before Binding : " + self.global_settings);
+        }
+        
+	self.onShowHideDryBox = function(event) {
+	  if ($('#drybox-history-div').is(':visible')) {
+		$('#drybox-history-div').hide();
+	  }
+          else {
+		$('#drybox-history-div').show();
+	  	self.draw_drybox_graph();
+	  }
+	}
+
+	self.showDryBoxHistText = function(event) {
+	   $('#drybox-history-scroll').toggle();
+	   $('#drybox-graph').toggle();
+	   $('#drybox-graph-legend').toggle();
+       self.printTextHistory();
+	   event.stopPropagation();
+	   
+	}
+
+	self.dryboxGraphTimeSpan = function(event) {
+	   event.stopPropagation();	   
+	   console.log(event.target.value);
+	   var canvas = document.getElementById("drybox-graph");
+
+	   $.ajax({
+		url:         "/api/plugin/drybox_sensor",
+		type:        "POST",
+		contentType: "application/json",
+		dataType:    "json",
+		headers:     {"X-Api-Key": UI_API_KEY},
+		data:        JSON.stringify({ "command": "graph_tspan", "tspan": event.target.value, "count":canvas.width }),
+		complete: function (data) {
+			console.log("POSt completed: ");
+			console.log(data.responseJSON);
+            self.tdata=data.responseJSON;
+            self.draw_drybox_graph();
+		}
+	   });
+	   //$.post("/api/plugin/drybox_sensor",'{"command":"graph_tspan","tspan":' + event.target + '}', function(resp) {
+	   //	   console.log(resp);
+	   //});
+	}
+		
+	self.draw_drybox_graph = function(){
+		
+	    var canvas = document.getElementById("drybox-graph");
+	    var cdiv = document.getElementById("drybox-history-div");
+	    canvas.width = cdiv.offsetWidth*.95;
+	    canvas.height = cdiv.offsetHeight*0.8;
+	    var context = canvas.getContext("2d");
+	    
+	    console.log("DRYBOX Canvas size: " + canvas.width + "x" + canvas.height);
+	    dp = []
+	    for (td in self.tdata) {
+		dp.push(self.tdata[td]['humid']);
+		dp.push(self.tdata[td]['temp']);
+	    }
+	    var maxv = Math.max(...dp);
+	    var minv = Math.min(...dp);
+	    var MAX_SCALE = Math.floor(maxv+(5-(maxv%5)));
+	    var MIN_SCALE = Math.floor(minv-(minv%5));
+	    
+	    var LABEL_WIDTH=60;
+	    var LABEL_HEIGHT=15;
+
+	    var graph_height = canvas.height - LABEL_HEIGHT;
+	    //clear the canvas
+	    context.clearRect(0, 0, canvas.width, canvas.height);
+	    context.moveTo(0, 0);
+	    
+	    //draw the graph line for humid data (blue)
+	    context.beginPath();
+	    context.lineWidth = 1;
+	    context.strokeStyle = "#0000ff";
+	    context.moveTo(0, canvas.height - ((graph_height * (self.tdata[0]['humid']-MIN_SCALE)) / (MAX_SCALE-MIN_SCALE))-LABEL_HEIGHT);
+	    for(var i = 0; i < self.tdata.length; i++){
+		context.lineTo((i+1) * ((canvas.width - LABEL_WIDTH) / self.tdata.length), canvas.height - ((graph_height * (self.tdata[i]['humid']-MIN_SCALE)) / (MAX_SCALE-MIN_SCALE)) - LABEL_HEIGHT);
+	    }
+	    context.stroke();
+
+	    //fill humid data
+	    context.fillStyle = "rgba(0, 0, 255, 0.1)";
+	    context.lineTo(canvas.width - LABEL_WIDTH-1, graph_height);
+	    context.lineTo(1, graph_height);
+	    context.closePath();
+	    context.fill();
+
+	    //draw the graph line for temp data
+	    context.beginPath();
+	    context.lineWidth = 1;
+	    context.strokeStyle = "#ff0000";
+	    context.moveTo(0, canvas.height - ((graph_height * (self.tdata[0]['temp']-MIN_SCALE)) / (MAX_SCALE-MIN_SCALE)) - LABEL_HEIGHT);
+	    for(i = 0; i < self.tdata.length; i++){
+		context.lineTo((i+1) * ((canvas.width - LABEL_WIDTH) / self.tdata.length), canvas.height - ((graph_height * (self.tdata[i]['temp']-MIN_SCALE)) / (MAX_SCALE-MIN_SCALE)) - LABEL_HEIGHT );
+	    }
+	    context.stroke();
+	    //fill data2
+	    context.fillStyle = "rgba(255, 0, 0, 0.1)";
+	    context.lineTo(canvas.width - LABEL_WIDTH - 1, graph_height);
+	    context.lineTo(1, graph_height);
+	    context.closePath();
+	    context.fill(); 
+	    
+
+	    //outline frame
+	    context.strokeStyle = "#222222";
+	    context.strokeRect(0, 0, canvas.width - LABEL_WIDTH, graph_height);
+	    
+	    //Vertical markers
+	    context.beginPath();
+	    context.strokeStyle = "#444444";
+	    context.lineWidth = 0.2;
+	    var horizontalLine = graph_height / 10;
+	   
+	    for (let index = 1; index < 10; index++) {
+		context.moveTo(0, horizontalLine * index);
+		context.lineTo(canvas.width - LABEL_WIDTH, (horizontalLine * index) + 0.2);
+		context.font = "14px arial";
+		context.fillStyle = "black"
+		context.fillText((MAX_SCALE - (index * ((MAX_SCALE-MIN_SCALE)/10))).toFixed(1) + "\u2103/%", canvas.width - LABEL_WIDTH + 3, (horizontalLine * index) + graph_height/50, LABEL_WIDTH);
+		context.moveTo(0, 0);
+		
+	    }
+	    context.fillText(MIN_SCALE.toFixed(1) + "\u2103/%", canvas.width - LABEL_WIDTH + 3, graph_height -0.8 );
+	    context.fillText(MAX_SCALE.toFixed(1) + "\u2103/%", canvas.width - LABEL_WIDTH + 3, 14);
+	    context.stroke();
+
+	    // Time Markers
+	    context.beginPath();
+	    context.strokeStyle = "#444444"
+	    context.lineWidth = 0.2;
+	    var lastmarkpos = -1;
+	    var lastmarkts = self.tdata[self.tdata.length-1]['ts'].valueOf();
+ 	    var dcount = 1
+	    for (let tindex = self.tdata.length-1; tindex >= 0 ; tindex--) {
+	      if (self.tdata[tindex]['ts'].valueOf() < (lastmarkts - 86400000)) {
+		 lastmarkts = self.tdata[tindex]['ts'].valueOf();
+                 context.moveTo((canvas.width-LABEL_WIDTH)/self.tdata.length * tindex, 0);
+		 context.lineTo((canvas.width-LABEL_WIDTH)/self.tdata.length * tindex, graph_height);
+	      	 context.font = "14px serif";
+	         context.fillStyle = "black";
+	      	 //var dstr = self.tsdata[tindex].oindex].getMonth()+1) + "\n" + self.tsdata[tindex].getHours() + ":" 
+		 //  + (self.tsdata[tindex].getMinutes() > 9 ? self.tsdata[tindex].getMinutes() : "0" + self.tsdata[tindex].getMinutes());   
+		 var dstr = "-1 day";
+		 
+		 if (dcount > 1) {
+	            dstr = "-" + dcount + "days";
+		 }
+		 dcount+=1;
+	         context.fillText(dstr,(canvas.width-LABEL_WIDTH)/self.tdata.length * tindex, canvas.height-5);	 
+	      }
+	    }
+	    context.stroke();
+	}
       }
 
       if (data.history) {
